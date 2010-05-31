66352dc64c943c01942799d334f9f2cdafa57d28
James S <i@nixeagle.org>
2010-05-31 17:25:00 -0400

    Source for my main website.

--- main/main.lisp
+++ main/main.lisp
@@ -1,1 +1,54 @@
 ;;; nothing for now until vhost is fixed up.
+(defpackage #:nixeagle.www.main
+  (:use :cl :hunchentoot :hunchentoot-vhost :nutils :cl-who :parenscript)
+  (:shadowing-import-from :parenscript :switch :var))
+
+(in-package :nixeagle.www.main)
+
+(defparameter *main*
+  (hunchentoot-vhost:make-virtual-host "main" "test.nixeagle.net"))
+
+(add-virtual-host *main* www::*nisp-1337-acceptor*)
+(add-virtual-host *main* www::*nisp-8080-acceptor*)
+(add-virtual-host *main* www::*nisp-6667-acceptor*)
+
+(define-easy-virtual-handler *main*
+    (front-page :uri "/")
+    ()
+  (with-html-output-to-string (*standard-output* nil :indent t)
+    (:html
+     (:head
+      (:title "nixeagle's site"))
+     (:body
+
+      (:p "My projects are:")
+      (:ul
+       (:li (htmlize-github-project "nisp" "Bunch of various lisp things."))
+       (:li (htmlize-github-project "nass" "Attempt at a lisp assembler. Sorta working 16 bit x86 assembly."))
+       (:li (htmlize-github-project "cl-github" "Implementation of the github v2 API in common lisp."))
+       (:li (htmlize-github-project "php-serialization" "Serialize to and from php's serialize() format."))
+       (:li (htmlize-github-project "convert" "Generic conversion utility for common lisp"))
+       (:li (htmlize-github-project "ooc-mode" "Emacs major mode for ooc-lang."))
+       (:li (htmlize-github-project "binary-data" "Common lisp binary data handling library."))
+       (:li (htmlize-github-project "loki" "Implementation of Ioke in common lisp."))
+       (:li (htmlize-github-project "nutils" "Common lisp utilities extending alexandria.")))))))
+
+(defun htmlize-github-project (name summary &key (stream *standard-output*))
+  "Generate html form suitable for a list entry."
+  (with-html-output (stream)
+    (:a :href (conc "http://github.com/nixeagle/" name) (str name))
+    ": " (str summary)))
+
+(with-output-to-string (*standard-output*)
+  (htmlize-github-project "hi" "hi"))
+
+(defun html-list (list &key (stream *standard-output*)
+                  (list-type :ul))
+  (declare ((member :ul :ol) list-type))
+  (with-html-output (stream)
+    (list-type
+     (dolist (item list)
+       (htm
+        (:li (str item)))))))
+
+;;; END
